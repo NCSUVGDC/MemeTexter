72d735e566609012509856d0b719a98ba71b0701
Joseph Dasilva <jojods1125@gmail.com>
2020-04-05 13:01:46 -0400

    Functionality for creating Chat, opening Chat, and going back

---
+++ MemeTexter/Assets/Scripts/UIBehaviors.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBehaviors : MonoBehaviour
+{
+    public static GameObject canvas;
+    public static GameObject chatsPage;
+    public static GameObject contentChats;
+
+    private void Awake()
+    {
+        if (canvas == null)
+        {
+            canvas = GameObject.Find("Canvas");
+        }
+
+        if (chatsPage == null)
+        {
+            chatsPage = GameObject.Find("RunningChats");
+        }
+
+        if (contentChats == null)
+        {
+            contentChats = GameObject.Find("ContentChats");
+        }
+
+        
+    }
+
+    public void OpenChat(Chat chat)
+    {
+        if (chat.otherPage == null)
+        {
+            GameObject newPage = (GameObject)Instantiate(Resources.Load("TestChat"));
+            newPage.transform.SetParent(canvas.transform, false);
+            chat.otherPage = newPage;
+        }
+        else
+        {
+            chat.otherPage.SetActive(true);
+        }
+    }
+
+    public void NewChat()
+    {
+        GameObject newChat = (GameObject)Instantiate(Resources.Load("ChatButton"));
+        newChat.transform.SetParent(contentChats.transform, false);
+    }
+
+    public void OpenRunningChats(GameObject currentPage)
+    {
+        chatsPage.SetActive(true);
+        currentPage.SetActive(false);
+    }
+}
