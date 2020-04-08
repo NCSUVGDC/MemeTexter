4b1e8fff344344b3cab3c4c2af3cfbda085b4338
Will <willcarpenter3@yahoo.com>
2020-04-07 21:58:02 -0400

    started skeleton of message sending

---
+++ MemeTexter/assets/Scripts/MessageBehaviors.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageBehaviors : MonoBehaviour
+{
+    public static GameObject canvas;
+    public static InputField textField;
+    public static GameObject contentMessages;
+
+    private void Awake()
+    {
+
+        if (canvas == null)
+        {
+            canvas = GameObject.Find("Canvas");
+        }
+
+        if (textField == null)
+        {
+            textField = GameObject.Find("TextField").GetComponent<InputField>();
+        }
+
+        if (contentMessages == null)
+        {
+            contentMessages = GameObject.Find("ContentMessages");
+        }
+
+    }
+
+    public void SendMessage()
+    {
+        GameObject newMessage = (GameObject)Instantiate(Resources.Load("TextMessage"));
+        newMessage.transform.SetParent(contentMessages.transform, false);
+
+        newMessage.GetComponentInChildren<Text>().text = textField.text;
+        textField.text = "";
+    }
+}
