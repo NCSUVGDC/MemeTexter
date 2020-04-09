ca219ec88ee43e1f89774e97895ff89ec789087f
Will <willcarpenter3@yahoo.com>
2020-04-08 23:14:38 -0400

    Fixed bugs in MessageBehavior, added KeyboardHandler (needs work still)

---
+++ MemeTexter/Assets/Scripts/KeyboardHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardHandler : MonoBehaviour
+{
+    Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = gameObject.GetComponent<RectTransform>().position;
+    }
+
+    private void Update()
+    {
+        if (TouchScreenKeyboard.visible)
+        {
+            transform.position = position + new Vector3(0, GetKeyboardHeight(true), 0);
+        } else
+        {
+            transform.position = position;
+        }
+    }
+
+    public static int GetKeyboardHeight(bool includeInput)
+    {
+#if UNITY_ANDROID
+        using (var unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            var unityPlayer = unityClass.GetStatic<AndroidJavaObject>("currentActivity").Get<AndroidJavaObject>("mUnityPlayer");
+            var view = unityPlayer.Call<AndroidJavaObject>("getView");
+            var dialog = unityPlayer.Get<AndroidJavaObject>("b");
+
+            if (view == null || dialog == null)
+                return 0;
+
+            var decorHeight = 0;
+
+            if (includeInput)
+            {
+                var decorView = dialog.Call<AndroidJavaObject>("getWindow").Call<AndroidJavaObject>("getDecorView");
+
+                if (decorView != null)
+                    decorHeight = decorView.Call<int>("getHeight");
+            }
+
+            using (var rect = new AndroidJavaObject("android.graphics.Rect"))
+            {
+                view.Call("getWindowVisibleDisplayFrame", rect);
+                return Display.main.systemHeight - rect.Call<int>("height") + decorHeight;
+            }
+        }
+#else
+        var height = Mathf.RoundToInt(TouchScreenKeyboard.area.height);
+        return height >= Display.main.systemHeight ? 0 : height;
+#endif
+    }
+}
--- MemeTexter/assets/Scripts/MessageBehaviors.cs
+++ MemeTexter/assets/Scripts/MessageBehaviors.cs
@@ -5,36 +5,18 @@ using UnityEngine.UI;
 
 public class MessageBehaviors : MonoBehaviour
 {
-    public static GameObject canvas;
-    public static InputField textField;
-    public static GameObject contentMessages;
-
-    private void Awake()
-    {
-
-        if (canvas == null)
-        {
-            canvas = GameObject.Find("Canvas");
-        }
-
-        if (textField == null)
-        {
-            textField = GameObject.Find("TextField").GetComponent<InputField>();
-        }
-
-        if (contentMessages == null)
-        {
-            contentMessages = GameObject.Find("ContentMessages");
-        }
-
-    }
+    public InputField textField;
+    public GameObject contentMessages;
 
     public void SendMessage()
     {
-        GameObject newMessage = (GameObject)Instantiate(Resources.Load("TextMessage"));
-        newMessage.transform.SetParent(contentMessages.transform, false);
+        if (textField.text != "")
+        {
+            GameObject newMessage = (GameObject)Instantiate(Resources.Load("TextMessage"));
+            newMessage.transform.SetParent(contentMessages.transform, false);
 
-        newMessage.GetComponentInChildren<Text>().text = textField.text;
-        textField.text = "";
+            newMessage.GetComponentInChildren<Text>().text = textField.text;
+            textField.text = "";
+        }
     }
 }
